384b939dbae334ed8e4f1bfd913b8bf9d22ba327
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-06-07 22:06:08 +0200

    fix: fix menu not collapsing on click

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
-var burgerMenu = document.getElementById('burger-menu');
+let menu = document.querySelector('.navigation-list');
 
-var overlay = document.getElementsByClassName('.navigation-list');
+function hideMenu() {
+    
+    menu.style.display = 'none';
+  }
 
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle('close');
-  overlay.classList.toggle('overlay');
-});
+  window.addEventListener('keydown', (e) => {
+    let menu = document.querySelector('.navigation-list');
+    if (e.key === 'Escape' && menu.classList.contains('navigation-list')) {
+      hideMenu();  
+    }
+  });
+
+  menu.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === menu) {
+      hideMenu();
+    }
+  });
+
+  function showMenu() {
+    let menu = document.querySelector('.navigation-list');
+    menu.style.display = 'flex';
+  }
+  let burgerMenu = document.querySelector('#hamburger');
+
+  burgerMenu.addEventListener('click', (e) => {
+    let menu = document.querySelector('.navigation-list');
+    
+      showMenu();  
+    
+  });
