7ceb9807959a86a372a671289e5ea938eff98c84
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-10-07 18:21:47 +0200

    feat: update styling of footer and navigation. test: comment in test code. file: replace logo

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@ let menu = document.querySelector('.navigation-list');
 //     // menu.style.display = 'none';
 //   }
 
-  window.addEventListener('keydown', (e) => {
-    let menu = document.querySelector('.navigation-list');
-    if (e.key === 'Escape' && menu.classList.contains('navigation-list')) {
-      hideMenu();  
-    }
-  });
+  // window.addEventListener('keydown', (e) => {
+  //   let menu = document.querySelector('.navigation-list');
+  //   if (e.key === 'Escape' && menu.classList.contains('navigation-list')) {
+  //     hideMenu();  
+  //   }
+  // });
 
-  menu.addEventListener('click', (e) => {
-    // Since this is also triggered when clicking INSIDE the modal
-    // We only want to close if the user clicks directly on the overlay
-    let target = e.target;
-    if (target === menu) {
-      hideMenu();
-    }
-  });
+  // menu.addEventListener('click', (e) => {
+  //   // Since this is also triggered when clicking INSIDE the modal
+  //   // We only want to close if the user clicks directly on the overlay
+  //   let target = e.target;
+  //   if (target === menu) {
+  //     hideMenu();
+  //   }
+  // });
 
   function showMenu() {
     let menu = document.querySelector('.navigation-list');
@@ -27,9 +27,9 @@ let menu = document.querySelector('.navigation-list');
   }
   let burgerMenu = document.querySelector('#hamburger');
 
-  burgerMenu.addEventListener('click', (e) => {
-    let menu = document.querySelector('.navigation-list');
+  // burgerMenu.addEventListener('click', (e) => {
+  //   let menu = document.querySelector('.navigation-list');
     
-      showMenu();  
+  //     showMenu();  
     
-  });
+  // });
