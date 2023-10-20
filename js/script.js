4584e6626fcdf5d95d1453c86e5693129082d704
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-10-20 20:49:04 +0200

    style: remove test code from script.js file

--- js/script.js
+++ js/script.js
@@ -1,25 +1,5 @@
 let menu = document.querySelector('.navigation-list');
 
-// function hideMenu() {
-    
-//     // menu.style.display = 'none';
-//   }
-
-  // window.addEventListener('keydown', (e) => {
-  //   let menu = document.querySelector('.navigation-list');
-  //   if (e.key === 'Escape' && menu.classList.contains('navigation-list')) {
-  //     hideMenu();  
-  //   }
-  // });
-
-  // menu.addEventListener('click', (e) => {
-  //   // Since this is also triggered when clicking INSIDE the modal
-  //   // We only want to close if the user clicks directly on the overlay
-  //   let target = e.target;
-  //   if (target === menu) {
-  //     hideMenu();
-  //   }
-  // });
 
   function showMenu() {
     let menu = document.querySelector('.navigation-list');
@@ -27,9 +7,3 @@ let menu = document.querySelector('.navigation-list');
   }
   let burgerMenu = document.querySelector('#hamburger');
 
-  // burgerMenu.addEventListener('click', (e) => {
-  //   let menu = document.querySelector('.navigation-list');
-    
-  //     showMenu();  
-    
-  // });
