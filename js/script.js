24ec6e91a1fb9b5ddbdd2c6d7d16b01aaa1805be
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-11-10 21:14:18 +0100

    feat: make collapsible menu close on menu and nav click

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 let menu = document.querySelector('.navigation-list');
+let burgerMenu = document.querySelector('.hamburger-container');
+let body = document.querySelector('body');
+// document.getElementById('hamburger').click(function(){
+//     menu.style.display = 'flex';
+//   });
+  
+//   document.querySelector('body').click(function(){
+//     menu.style.display = 'none';
+//  });
 
+// function hideMenu () {
+//   menu.style.right = "-100%";
+   
 
-  function showMenu() {
-    let menu = document.querySelector('.navigation-list');
-    menu.style.display = 'flex';
+//   }
+  
+   
+  
+  
+
+function toggleMenu () {
+  // get the menu element by its id
+  // check if the menu is visible or not
+  if (menu.style.right == "-100%") {
+    // if the menu is visible, hide it
+    menu.style.right = "0%";
   }
-  let burgerMenu = document.querySelector('#hamburger');
+  else {
+    // if the menu is hidden, show it
+    menu.style.right = "-100%";
+  }
+}
+
+
 
