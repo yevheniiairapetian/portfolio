d9181643d0539bf38e4b7af8bb35ad292e9cbca6
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-11-11 01:53:06 +0100

    feat: update styling, autoprefix css

--- js/script.js
+++ js/script.js
@@ -1,37 +1,20 @@
 let menu = document.querySelector('.navigation-list');
 let burgerMenu = document.querySelector('.hamburger-container');
 let body = document.querySelector('body');
-// document.getElementById('hamburger').click(function(){
-//     menu.style.display = 'flex';
-//   });
-  
-//   document.querySelector('body').click(function(){
-//     menu.style.display = 'none';
-//  });
-
-// function hideMenu () {
-//   menu.style.right = "-100%";
-   
-
-//   }
-  
-   
-  
-  
 
 function toggleMenu () {
-  // get the menu element by its id
   // check if the menu is visible or not
   if (menu.style.right == "-100%") {
-    // if the menu is visible, hide it
+    // if the menu is hidden, show it
     menu.style.right = "0%";
   }
   else {
-    // if the menu is hidden, show it
+    // if the menu is visible, hide it
     menu.style.right = "-100%";
   }
 }
 
+// slider
 
 var slideIndex = 1;
               showDivs(slideIndex);
