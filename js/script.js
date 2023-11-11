82603c414983261f8181158c803d7e8eb0ae9627
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-11-11 13:54:39 +0100

    feat: update styling, refactor js file

--- js/script.js
+++ js/script.js
@@ -30,15 +30,17 @@ var slideIndex = 1;
               function showDivs(n) {
                 var i;
                 var x = document.getElementsByClassName("slides");
-                var dots = document.getElementsByClassName("slide-counts");
+                // var dots = document.getElementsByClassName("slide-counts");
                 if (n > x.length) {slideIndex = 1}    
                 if (n < 1) {slideIndex = x.length}
                 for (i = 0; i < x.length; i++) {
-                  x[i].style.display = "none";  
+                  x[i].style.display = "none"; 
                 }
-                for (i = 0; i < dots.length; i++) {
-                  dots[i].className = dots[i].className.replace(" w3-red", "");
-                }
-                x[slideIndex-1].style.display = "block";  
-                dots[slideIndex-1].className += " w3-red";
+                // for (i = 0; i < dots.length; i++) {
+                //   dots[i].className = dots[i].className.replace(" w3-red", "");
+                // }
+                x[slideIndex-1].style.display = "block"; 
+                // dots[slideIndex-1].className += " w3-red";
               }
+
+              
