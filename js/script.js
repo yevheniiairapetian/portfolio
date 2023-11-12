e66e1e831fd6ebc6fd52f03d00a554f352febac3
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-11-12 23:18:15 +0100

    fix: fix slider error when clicking on project info button

--- js/script.js
+++ js/script.js
@@ -50,17 +50,12 @@ function currentDiv(n) {
 function showDivs(n) {
   let i;
   let x = document.getElementsByClassName("slides");
-  // var dots = document.getElementsByClassName("slide-counts");
   if (n > x.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = x.length }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
+    x[slideIndex - 1].style.display = "block";
   }
-  // for (i = 0; i < dots.length; i++) {
-  //   dots[i].className = dots[i].className.replace(" w3-red", "");
-  // }
-  x[slideIndex - 1].style.display = "block";
-  // dots[slideIndex-1].className += " w3-red";
 }
 
 // projects more buttons
