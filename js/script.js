f911995b28e8a72b53371a446e53e45ce320897f
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-01-27 18:24:44 +0100

    feat: localize about me page and add language button to about me page menu

--- js/script.js
+++ js/script.js
@@ -69,6 +69,153 @@ function showMenuDownloadLinks(){
   
 }
 
+function showLocalize(){
+  let DE = $('.german');
+  let EN = $('.english');
+  let UK = $('.ukrainian');
+  let caret = $('.localize-caret');
+  let langs = $('.langs');
+  // if (linkDE.style.display == "none" && linkEN.style.display == "none" && linkUK.style.display == "none") {
+    DE.fadeToggle();
+    
+    EN.fadeToggle();
+    UK.fadeToggle();
+    caret.fadeToggle();
+  // }else{
+    
+  // }
+  
+}
+
+
+let langs = document.querySelector(".langs"),
+     link = document.querySelectorAll(".languages"),
+     title = document.querySelector(".websiteTitleAbout"),
+     about = document.querySelector(".aboutMenu"),
+     projects = document.querySelector(".projectsMenu"),
+     testimonials = document.querySelector(".testimonialsMenu"),
+     contact = document.querySelector(".contactMenu"),
+     resume = document.querySelector(".resumeMenu"),
+     hello = document.querySelector(".hello"),
+     introduction = document.querySelector(".introduction"),
+     prevExperience = document.querySelector(".prevExperience"),
+     myFocus = document.querySelector(".myFocus"),
+     mySecondFocus = document.querySelector(".mySecondFocus"),
+     availability = document.querySelector(".availability"),
+     hobbies = document.querySelector(".hobbies"),
+     checkOutBig = document.querySelector(".checkOutBig"),
+     checkOutSmall = document.querySelector(".checkOutSmall"),
+     resumeBig = document.querySelector(".resumeBig"),
+     resumeSmall = document.querySelector(".resumeSmall"),
+     copyright = document.querySelector(".copyright"),
+     impressum = document.querySelector(".impressum")
+
+
+link.forEach(el=>{
+     el.addEventListener("click", ()=>{
+          langs.querySelector(".active").classList.remove("active");
+          el.classList.add("active");
+
+          let attr = el.getAttribute("language")
+
+          title.textContent = data[attr].websiteTitleAbout;
+          about.textContent = data[attr].aboutMenu;
+          projects.textContent = data[attr].projectsMenu;
+          testimonials.textContent = data[attr].testimonialsMenu;
+          contact.textContent = data[attr].contactMenu;
+          resume.textContent = data[attr].resumeMenu;
+          hello.textContent = data[attr].hello;
+          introduction.textContent = data[attr].introduction;
+          prevExperience.textContent = data[attr].prevExperience;
+          myFocus.textContent = data[attr].myFocus;
+          mySecondFocus.textContent = data[attr].mySecondFocus;
+          availability.textContent = data[attr].availability;
+          hobbies.textContent = data[attr].hobbies;
+          checkOutBig.textContent = data[attr].checkOutBig;
+          checkOutSmall.textContent = data[attr].checkOutSmall;
+          resumeBig.textContent = data[attr].resumeBig;
+          resumeSmall.textContent = data[attr].resumeSmall;
+          copyright.textContent = data[attr].copyright;
+          impressum.textContent = data[attr].impressum;
+
+          
+        
+     })
+})
+
+let data = {
+     german: {
+      websiteTitleAbout:"Yevhenii Airapetian - About",
+      aboutMenu:"Über",
+      projectsMenu:"Projekte",
+      testimonialsMenu:"Empfehlungsschreiben",
+      contactMenu:"Kontakt",
+      resumeMenu:"Lebenslauf",
+      hello: "Hallo, ich bin Yevhenii!",
+      introduction: "Ich bin ein Webentwickler mit Leidenschaft für Technologie",
+      prevExperience: "Nachdem ich bereits Erfahrung im Unterrichten von Fremdsprachen und im wissenschaftlichen Schreiben gesammelt habe, freue ich mich darauf, eine Karriere als Webentwickler anzustreben.",
+      myFocus:"Ich habe mich immer darauf konzentriert, qualitativ hochwertige Arbeit zu liefern, Fristen einzuhalten und meine ausgefeilten Fähigkeiten zur Problemlösung, zum Zeitmanagement und zum logischen Denken einzusetzen.",
+      mySecondFocus:"Mein Fokus liegt darauf, mein Fachwissen mit meinen beruflichen Bestrebungen zu kombinieren, um funktionale, reaktionsschnelle und zugängliche Softwarelösungen zu schaffen.",
+      availability:"Ich stehe für Vorort- und Hybrideinsätze im Raum Berlin und Potsdam oder remote zur Verfügung.",
+      hobbies:"Neben meinen Leidenschaften in der Webentwicklung bin ich auch ein aktiver Hobbyfotograf und Fitness-Enthusiast, und an jedem anderen Tag kann man mich draußen beim Fotografieren oder Heben schwerer Gewichte antreffen.",
+      checkOutBig:"Code",
+      checkOutSmall:"Code",
+      resumeBig:"Lebenslauf",
+      resumeSmall:"CV",
+      copyright:"© Yevhenii Airapetian. Alle Rechte vorbehalten.",
+      impressum:"Impressum",
+     },
+     english: {
+          websiteTitleAbout:"Yevhenii Airapetian - About",
+          aboutMenu:"About",
+          projectsMenu:"Projects",
+          testimonialsMenu:"Testimonials",
+          contactMenu:"Contact",
+          resumeMenu:"Resume",
+          hello: "Hi, I'm Yevhenii!",
+          introduction: "I'm a web developer passionate about technology",
+          prevExperience: "With previous experience in teaching foreign languages and academic writing, I'm excited to pursue a career as a web developer.",
+          myFocus:"I have always focused on delivering high-quality work that met deadlines, employing my refined problem-solving, time-management, and logical thinking skills.",
+          mySecondFocus:"My focus is to combine my expertise with my professional aspirations to help create functional, responsive, and accessible software solutions.",
+          availability:"I'm available for on-site and hybrid employment in the Berlin and Potsdam area or remotely.",
+          hobbies:"In addition to my passions in web development, I'm also an active hobby photographer and fitness enthusiast, and any other day you can find me outside taking photos or lifting heavy weights.",
+          checkOutBig:"Check out Code",
+          checkOutSmall:"Code",
+          resumeBig:"Download Resume",
+          resumeSmall:"Resume",
+          copyright:"© Yevhenii Airapetian. All rights reserved.",
+          impressum:"Impressum",
+
+
+
+
+
+
+
+
+     },
+     ukrainian: {
+      websiteTitleAbout:"Євгеній Айрапетян - Про мене",
+      aboutMenu:"Про мене",
+      projectsMenu:"Проекти",
+      testimonialsMenu:"Відгуки",
+      contactMenu:"Зв'язок",
+      resumeMenu:"Резюме",
+      hello: "Привіт, мене звати Євгеній!",
+      introduction: "Я веб-розробник і захоплююся технологіями",
+      prevExperience: "Маючи попередній досвід викладання іноземних мов та академічного письма, я з радістю продовжую кар'єру веб-розробника.",
+      myFocus:"Я завжди зосереджувався на виконанні високоякісної роботи, дотримуючись дедлайнів, використовуючи мої навички вирішення проблем, тайм-менеджмент і логічне мислення.",
+      mySecondFocus:"Я зосереджений на тому, щоб об'єднати свій досвід роботи із професійними прагненнями, щоб допомагати створювати функціональні, адаптивні та доступні програмні рішення.",
+      availability:"Я доступний для роботи в офісі та в гібридному режимі в районі Берліна та Потсдама або віддалено.",
+      hobbies:"Окрім моєї пристрасті до веб-розробки, я також активний фотограф-любитель та фітнес ентузіаст, і у вільний час Ви можете побачити мене на вулиці, де я фотографую або займаюсь фітнесом.",
+      checkOutBig:"Глянути Код",
+      checkOutSmall:"Код",
+      resumeBig:"Завантажити резюме",
+      resumeSmall:"Резюме",
+      copyright:"© Євгеній Айрапетян. Усі права захищено.",
+      impressum:"Відбиток",          
+     }
+}
 
 // toggles menu
 function toggleMenu() {
