d7492a3ab5ac2416e8c090c5f7d5f433962caf9c
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-03-16 00:03:21 +0100

    feat: add footer functionality and styling

--- portfolio/src/components/main-view/main-view.jsx
+++ portfolio/src/components/main-view/main-view.jsx
@@ -45,7 +45,7 @@ export const MainView = () =>{
                       <AboutView
                         
                       />
-                      {/* <Footer /> */}
+                       <Footer /> 
                     </Col>
                   
                 
