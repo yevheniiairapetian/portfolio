4e7a13f902a5158215fe222592fe4041458a0973
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-04-21 00:24:35 +0200

    fix: fixed navbar resume download behavior

--- portfolio/src/components/navigation-bar/navigation-bar.jsx
+++ portfolio/src/components/navigation-bar/navigation-bar.jsx
@@ -72,16 +72,16 @@ export const NavigationBar = () => {
 			  
             
             
-              <NavDropdown.Item download="Yevhenii-Airapetian-DE" href="#action/3.1"><Image roundedCircle className="flags mt-3 me-3" src={deImg} alt="An icon showing the German flag"/>
-			  <Link to="img/germany.svg" title="Download the resume version in German" className="download-de" target="_blank" download="Yevhenii-Airapetian-DE"></Link>
+               <NavDropdown.Item title="Klicken Sie hier, um einen externen Link mit der deutschen Lebenslaufversion zu öffnen" download="Yevhenii-Airapetian-DE" href="https://www.dropbox.com/scl/fi/kzn7bsab05ddugcjrkjub/Yevhenii-Airapetian-DE.pdf?rlkey=sam0r180x4qfjn7ey2rv5hd03&st=rljwq2uz&dl=0" target="_blank"><Image roundedCircle className="flags mt-3 me-3" src={deImg} alt="An icon showing the German flag"/>
+			  <Link to={"https://file.io/Mck4WxEzQGAu"} title="Download the resume version in German" className="download-de" target="_blank" ></Link>
               </NavDropdown.Item>
-              <NavDropdown.Item download="Yevhenii-Airapetian-EN" href="#action/3.2">
+              <NavDropdown.Item title="Click to open an external link with the English CV version" download="Yevhenii-Airapetian-EN" href="https://www.dropbox.com/scl/fi/y1s7krvn2tcri1495fdyb/Yevhenii-Airapetian-EN.pdf?rlkey=hxo8aghbdcdysd8otdd57c1t1&st=t0wgsfx3&dl=0" target="_blank">
 			  <Image roundedCircle className="flags mt-3 me-3" src={usImg} alt="An icon showing the USA flag"/>
-			  <Link to="img/united-states.svg" title="Download the resume version in English" className="download-en" target="_blank" download="Yevhenii-Airapetian-EN"></Link>
+			  <Link to={"https://file.io/HVTDNPEXVS7b"} title="Download the resume version in English" className="download-en" target="_blank" download="Yevhenii-Airapetian-EN"></Link>
 			  </NavDropdown.Item>
-              <NavDropdown.Item download="Yevhenii-Airapetian-UK" href="#action/3.3">
+              <NavDropdown.Item title="Натисніть, щоб відкрити зовнішнє посилання з українською версією резюме" download="Yevhenii-Airapetian-UK" href="https://www.dropbox.com/scl/fi/k1z3vwrnxmqux24j1m49r/Yevhenii-Airapetian-UA.pdf?rlkey=xy4vqr68rcljth5z23kp6wu7m&st=n0kmkcas&dl=0" target="_blank">
 			  <Image roundedCircle className="flags mt-3 me-3" src={ukraineImg} alt="An icon showing Ukrainian flag"/>
-			  <Link to="img/ukraine.svg" title="Download the resume version in Ukrainian" className="download-uk" target="_blank" download="Yevhenii-Airapetian-UK"></Link>
+			  <Link to={"https://file.io/fsghnXVPdFna"} title="Download the resume zip folder" className="download-uk" target="_blank" download="https://file.io/fsghnXVPdFna"></Link>
               </NavDropdown.Item>
             </NavDropdown>
 								
