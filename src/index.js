db3120c960776ad63a4f9666ed320e9b46976121
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-04-22 22:36:34 +0200

    feat: update router in main view

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import {MainView} from '../src/components/main-view/main-view';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <MainView />
-  </React.StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
