b2ac785c8746d7c1fcf43396ff8dc04b796d5180
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-04-23 22:57:52 +0200

    feat: localize projects view

--- src/components/projects-view/projects-view.jsx
+++ src/components/projects-view/projects-view.jsx
@@ -1,22 +1,22 @@
 
 import { Navbar, Container, Row, Col, Nav, Image } from "react-bootstrap";
 import { ProjectsSlider } from "../projects-slider/projects-slider";
+import { useTranslation } from 'react-i18next';
 import projectsPicture from "./img/projects.jpg";
 import projectsViewCSS from "./css/projectsview.css";
 export const ProjectsView = () => {
+        const { t } = useTranslation();
  return(
 <Container>
         <Row>
             <Col>
 <div className="app-container" onclick="hideMenu(); closeSidebar(); closeMenuDownloadLinks()">
-                <h1 className="projects-main-heading mb-4">Projects</h1>
-                <p className="projects-overview-paragraph">Here you will find the current web development projects that I completed
-                and worked on. All of them have hyperlinks to the codebase and the project cases, most have a link to
-                the deployed version, and some also have additional links.</p>
+                <h1 className="projects-main-heading mb-4">{t("projects")}</h1>
+                <p className="projects-overview-paragraph">{t("projectsInfo")}</p>
                 <Image className="projects-image" src={projectsPicture} alt="An image showing a team working" />
 
 
-                <p className="slider-overview-paragraph center fade-in-up-on-scroll">Use the slider controls to click through the projects list.</p>
+                <p className="slider-overview-paragraph center fade-in-up-on-scroll">{t("projectsViewTip")}</p>
 
                 
                 {/* <div class="slider-outer-wrapper fade-in-up-on-scroll">
