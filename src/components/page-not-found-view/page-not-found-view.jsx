98ec20d39b5cf77554d2ac87aaca8ce7eb61e664
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-04-25 14:56:55 +0200

    feat: localize 404 view

--- src/components/page-not-found-view/page-not-found-view.jsx
+++ src/components/page-not-found-view/page-not-found-view.jsx
@@ -4,16 +4,18 @@ import pageNotFoundViewCSS from "./css/404.css";
 import image404 from './img/404.gif';
 import { Link } from "react-router-dom";
 import {Footer} from "../footer/footer";
+import { useTranslation } from 'react-i18next';
 
 export const PageNotFoundView = () => {
+    const { t } = useTranslation();
     return(
         <Container>
             <Row>
                 <Col xs={10} sm={10} md={10} className="text-center m-auto" >
         <Image src={image404} className="picture-404 m-auto mt-5 mb-5" alt="An image showing an old TV" />
         <div className="main-information_container">
-            <h1>We couldn't find the resource you were looking for.</h1>
-            <p className="overview-404">But you can always return to the <Link to="/">home page</Link></p>
+            <h1>{t("notFound")}</h1>
+            <p className="overview-404">{t("returnHomeParagraph")}<Link to="/">{t("returnHomeLink")}</Link></p>
 
         </div>
                 </Col>
