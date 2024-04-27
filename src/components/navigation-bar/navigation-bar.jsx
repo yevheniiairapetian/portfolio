315069eb3833c8cf8a1293b0f98b16d933a7d82b
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-04-28 00:36:11 +0200

    feat: add autocollapse on link click in nav  functionality

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -15,7 +15,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const NavigationBar = () => {
 	const [isDarkMode, setDarkMode] = useDarkMode();
 	const { t, i18n } = useTranslation();
-
+	const [expanded, setExpanded] = useState(false);
 	function showMenuDownloadLinks() {
 		let linkDE = $('.download-menu-de');
 		let linkEN = $('.download-menu-en');
@@ -76,24 +76,24 @@ export const NavigationBar = () => {
 
 	return (
 
-		<Navbar onClick={() => { closeMenuDownloadLinks() }} className="page-header" expand="lg" id="navigation">
+		<Navbar expanded={expanded} onClick={() => { closeMenuDownloadLinks() }} className="page-header" expand="lg" id="navigation">
 			<Container className="navigation">
 				<Navbar.Brand className="p-2 brand" as={Link} to="/" expand="lg">
 					{/* <Nav.Link className="" as={Link} to='/'> */}
-					<Image className="img-responsive logo" alt="logo" src={"../../logo.svg"} />
+					<Image onClick={() => setExpanded(false)} className="img-responsive logo" alt="logo" src={"../../logo.svg"} />
 					{/* </Nav.Link> */}
 
 
 
 				</Navbar.Brand>
-				<Navbar.Toggle />
+				<Navbar.Toggle onClick={() => setExpanded(!expanded)} />
 				<Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
 					<Nav>
 
-						<Nav.Link className="text-light pe-4" as={Link} to='/'>
+						<Nav.Link onClick={() => setExpanded(false)} className="text-light pe-4" as={Link} to='/'>
 							{t("menu.aboutMenu")}
 						</Nav.Link>
-						<Nav.Link className="text-light pe-4" as={Link} to='/projects'>
+						<Nav.Link onClick={() => setExpanded(false)} className="text-light pe-4" as={Link} to='/projects'>
 							{t("menu.projectsMenu")}
 
 						</Nav.Link>
@@ -103,29 +103,29 @@ export const NavigationBar = () => {
 
 
 							<NavDropdown.Item target="_self" alt="">
-								<Link to="./../r3play-case" className="nav-cases" target="_self" >{t("menu.r3playMenu")}</Link>
+								<Link onClick={() => setExpanded(false)} to="./../r3play-case" className="nav-cases" target="_self" >{t("menu.r3playMenu")}</Link>
 							</NavDropdown.Item>
 							<NavDropdown.Item target="_self" alt="">
-								<Link to="./../myFlix-case" className="nav-cases" target="_self" >{t("menu.myFlixMenu")}</Link>
+								<Link onClick={() => setExpanded(false)} to="./../myFlix-case" className="nav-cases" target="_self" >{t("menu.myFlixMenu")}</Link>
 							</NavDropdown.Item>
 							<NavDropdown.Item target="_self" alt="">
-								<Link to="./../meet-case" className="nav-cases" target="_self" >{t("menu.meetMenu")}</Link>
+								<Link onClick={() => setExpanded(false)} to="./../meet-case" className="nav-cases" target="_self" >{t("menu.meetMenu")}</Link>
 							</NavDropdown.Item>
 							<NavDropdown.Item target="_self" alt="">
-								<Link to="./../owrite-case" className="nav-cases" target="_self" >{t("menu.owriteMenu")}</Link>
+								<Link onClick={() => setExpanded(false)} to="./../owrite-case" className="nav-cases" target="_self" >{t("menu.owriteMenu")}</Link>
 							</NavDropdown.Item>
 							<NavDropdown.Item target="_self" alt="">
-								<Link to="./../pokemon-case" className="nav-cases" target="_self" >{t("menu.pokemonMenu")}</Link>
+								<Link onClick={() => setExpanded(false)} to="./../pokemon-case" className="nav-cases" target="_self" >{t("menu.pokemonMenu")}</Link>
 							</NavDropdown.Item>
 							<NavDropdown.Item target="_self" alt="">
-								<Link to="./../to-do-case" className="nav-cases" target="_self" >{t("menu.toDoMenu")}</Link>
+								<Link onClick={() => setExpanded(false)} to="./../to-do-case" className="nav-cases" target="_self" >{t("menu.toDoMenu")}</Link>
 							</NavDropdown.Item>
 
 						</NavDropdown>
-						<Nav.Link className="text-light pe-4" as={Link} to='/testimonials'>
+						<Nav.Link onClick={() => setExpanded(false)} className="text-light pe-4" as={Link} to='/testimonials'>
 							{t("menu.testimonialsMenu")}
 						</Nav.Link>
-						<Nav.Link className="text-light pe-4" as={Link} to='/contact'>
+						<Nav.Link onClick={() => setExpanded(false)} className="text-light pe-4" as={Link} to='/contact'>
 							{t("menu.contactMenu")}
 						</Nav.Link>
 						<NavDropdown onClick={() => { showMenuDownloadLinks() }} title={t("menu.resumeMenu")} id="collapsible-nav-dropdown">
@@ -136,17 +136,17 @@ export const NavigationBar = () => {
 							<NavDropdown.Item className="text-center" title="Klicken Sie hier, um meinen Lebenslauf auf Deutsch herunterzuladen. Möglicherweise müssen Sie einen Dateibetrachter wie Adobe Acrobat oder ähnliches verwenden" download="Yevhenii-Airapetian-DE" href="https://www.dropbox.com/scl/fi/kzn7bsab05ddugcjrkjub/Yevhenii-Airapetian-DE.pdf?rlkey=sam0r180x4qfjn7ey2rv5hd03&st=rljwq2uz&dl=0" target="_blank">
 								{/* <Image roundedCircle className="flags mt-3 me-3 nav-lang-cases" src={deImg} alt="An icon showing the German flag" /> */}
 								<img className="flags mt-3 me-3" alt="An icon showing the German flag" src="https://hatscripts.github.io/circle-flags/flags/de.svg" width="28"/>
-								<Link to={"https://file.io/Mck4WxEzQGAu"} title="Download the resume version in German" className="download-de" target="_blank" ></Link>
+								<Link onClick={() => setExpanded(false)} to={"https://file.io/Mck4WxEzQGAu"} title="Download the resume version in German" className="download-de" target="_blank" ></Link>
 							</NavDropdown.Item>
 							<NavDropdown.Item className="text-center" title="Click here to download my resume in English. You might need to use a file viewer like Adobe Acrobat or similar" download="Yevhenii-Airapetian-EN" href="https://www.dropbox.com/scl/fi/y1s7krvn2tcri1495fdyb/Yevhenii-Airapetian-EN.pdf?rlkey=hxo8aghbdcdysd8otdd57c1t1&st=t0wgsfx3&dl=0" target="_blank">
 								{/* <Image roundedCircle className="flags mt-3 me-3 nav-lang-cases" src={usImg} alt="An icon showing the USA flag" /> */}
 								<img className="flags mt-3 me-3" alt="An icon showing the USA flag" src="https://hatscripts.github.io/circle-flags/flags/us.svg" width="28"/>
-								<Link to={"https://file.io/HVTDNPEXVS7b"} title="Download the resume version in English" className="download-en" target="_blank" download="Yevhenii-Airapetian-EN"></Link>
+								<Link onClick={() => setExpanded(false)} to={"https://file.io/HVTDNPEXVS7b"} title="Download the resume version in English" className="download-en" target="_blank" download="Yevhenii-Airapetian-EN"></Link>
 							</NavDropdown.Item>
 							<NavDropdown.Item className="text-center" title="Натисніть тут, щоб завантажити моє резюме українською мовою. Можливо, вам знадобиться програма для перегляду файлів, наприклад Adobe Acrobat або подібна" download="Yevhenii-Airapetian-UK" href="https://www.dropbox.com/scl/fi/k1z3vwrnxmqux24j1m49r/Yevhenii-Airapetian-UA.pdf?rlkey=xy4vqr68rcljth5z23kp6wu7m&st=n0kmkcas&dl=0" target="_blank">
 								{/* <Image roundedCircle className="flags mt-3 me-3 nav-lang-cases" src={ukraineImg} alt="An icon showing Ukrainian flag" /> */}
 								<img className="flags mt-3 me-3" alt="An icon showing Ukrainian flag" src="https://hatscripts.github.io/circle-flags/flags/ua.svg" width="28"/>
-								<Link to={"https://file.io/fsghnXVPdFna"} title="Download the resume zip folder" className="download-uk" target="_blank" download="https://file.io/fsghnXVPdFna"></Link>
+								<Link onClick={() => setExpanded(false)} to={"https://file.io/fsghnXVPdFna"} title="Download the resume zip folder" className="download-uk" target="_blank" download="https://file.io/fsghnXVPdFna"></Link>
 							</NavDropdown.Item>
 
 						</NavDropdown>
